76a56a53c4d73a84dbb66e583544d361ea537b29
shandev <shanrep@Sivas-MacBook-Air.local>
2015-01-23 01:29:22 +0530

    Added FrogJump
    Yet to complete the solution

---
+++ FrogJump/FrogJump/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  FrogJump
+//
+//  Created by Shan on 23/01/15.
+//  Copyright (c) 2015 Shan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
